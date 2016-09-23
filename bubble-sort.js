0d6c60c4f47a5e7e107fdb9b8bdc625b503781b1
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-23 11:31:36 -1000

    passed first test, stubbed out a method for bubbleSort

---
+++ bubble-sort.js
@@ -0,0 +1,12 @@
+function bubbleSort(array) {
+  passCount = 0;
+  var sorted = false;
+
+  while(!sorted) {
+    sorted = true;
+  }
+
+  return passCount;
+}
+
+module.exports = bubbleSort;
--- test/bubble-sort.spec.js
+++ test/bubble-sort.spec.js
@@ -5,10 +5,11 @@ const bubbleSort = require('../bubble-sort.js');
 describe('bubbleSort', function() {
 
   it('should be a function', function() {
-    expect(bubbleSort).to.not.be.undefined;
+    //expect(bubbleSort).to.not.be.undefined;
     expect(bubbleSort).to.be.a('function');
   });
-  it('that sorts an array and returns the number of passes required to sort it', function() {
+
+  it('returns the number of passes required to sort it', function() {
     expect(bubbleSort([5, 1, 4, 2, 8])).to.equal(3);
   });
 
