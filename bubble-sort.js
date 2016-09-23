14512bbf81ef26e3df42c1ae413affd18ea6633a
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-23 11:53:55 -1000

    passed first test

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,8 +2,28 @@ function bubbleSort(array) {
   passCount = 0;
   var sorted = false;
 
+  var prev = -Infinity;
+  function isBiggerThanPrev(elem) {
+    var result = (elem >= prev);
+    prev = elem;
+    return result;
+  }
+
   while(!sorted) {
-    sorted = true;
+
+    for(var i = 0; i < (array.length - 1); i++){
+      if(array[i] > array[i + 1]) {
+        array.splice(i, 0, array.splice((i + 1), 1)[0]);
+      }
+    }
+    passCount++;
+
+    prev = -Infinity;
+    if(array.every(isBiggerThanPrev)) {
+      passCount++;
+      sorted = true;
+    }
+
   }
 
   return passCount;
